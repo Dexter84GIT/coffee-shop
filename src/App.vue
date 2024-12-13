1e2a3613ad3089d1cd3316cbde1d0b7095185aaa
Dexter84 <fenixzaec84@gmail.com>
2024-12-13 21:35:13 +0700

    lesson02 complete

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    Hello Vue.js
+  <div class="wrapper">
+    <hero-view />
+    <footer-component />
   </div>
 </template>
 
 <script>
-
+import HeroView from '@/views/HeroView.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 export default {
-
+  components: { HeroView, FooterComponent }
 }
 </script>
 
-<style>
-
-</style>
+<style lang="scss" scoped></style>
