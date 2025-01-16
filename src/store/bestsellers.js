735d8d88404623007405f7d838b5405a12d46421
Dexter84 <fenixzaec84@gmail.com>
2025-01-16 17:42:24 +0700

    lesson11 completed

--- src/store/bestsellers.js
+++ src/store/bestsellers.js
@@ -1,28 +1,6 @@
 const bestsellers = {
     state: {
-        bestsellers: [
-            {
-                id: 0,
-                image: 'coffee-1.jpg',
-                name: 'Solimo Coffee Beans 22kg',
-                price: 10.73,
-                className: 'best__item',
-            },
-            {
-                id: 1,
-                image: 'coffee-2.jpg',
-                name: 'Presto Coffee Beans 1kg',
-                price: 15.99,
-                className: 'best__item',
-            },
-            {
-                id: 2,
-                image: 'coffee-3.jpg',
-                name: 'AROMISTICO Coffee 1kg',
-                price: 7.99,
-                className: 'best__item',
-            },
-        ]
+        bestsellers: []
     },
     actions: {
         setSestsellersData({ commit }, data) {
